bdb5f2027e76a60e1fdd032f78ac85af73378f5a
pp449 <mma7710@naver.com>
2023-03-28 15:20:08 +0900

    Chore: App 테스트에서 간단한 Home 테스트로 변경

---
+++ src/components/FooterTab/index.tsx
@@ -0,0 +1,5 @@
+const FooterTab = () => {
+  return <h1>FooterTab</h1>;
+};
+
+export default FooterTab;
--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,13 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import React from 'react';
 
-import App from './App';
+import Home from './index';
 
 describe('App 컴포넌트', () => {
   it('페이지에 "App"이라는 텍스트가 렌더링된다.', () => {
-    render(<App />);
-    const textElement = screen.getByText(/App/i);
+    render(<Home />);
+    const textElement = screen.getByText(/메인/i);
     expect(textElement).toBeInTheDocument();
   });
 });
