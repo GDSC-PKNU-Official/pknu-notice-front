14d7307acf79c573357ee2e105cb75842e53c0b9
dohun <dohun31@naver.com>
2023-03-28 14:05:49 +0900

    Feat: Router 적용

---
+++ src/pages/Announcement/index.tsx
@@ -0,0 +1,5 @@
+const Announcement = () => {
+  return <h1>공지사항 페이지</h1>;
+};
+
+export default Announcement;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>메인 페이지</h1>;
+};
+
+export default Home;
