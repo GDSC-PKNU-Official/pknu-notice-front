abd7c9ab5174377616f376a04fb01567450eace2
hwinkr <dnddl8280@naver.com>
2023-03-28 14:32:44 +0900

    Refactor: Image 컴포넌트 수정

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,61 +1,44 @@
 import styled from '@emotion/styled';
+import { setSize } from '@utils/styles/size';
 import React, { CSSProperties } from 'react';
-
-type Size = 'large' | 'medium' | 'small';
+import { Size, SizeOption } from 'src/@types/styles/size';
 
 type ImageSize = {
-  [key in Size]: {
-    height: CSSProperties['height'];
-    width: CSSProperties['width'];
-  };
+  [key in SizeOption]: Size;
 };
 
-const large = '250 px' as CSSProperties['height' | 'width'];
-const medium = '200 px' as CSSProperties['height' | 'width'];
-const small = '150 px' as CSSProperties['height' | 'width'];
-
 const imageSize: ImageSize = {
-  large: {
-    height: large,
-    width: large,
-  },
-  medium: {
-    height: medium,
-    width: medium,
-  },
-  small: {
-    height: small,
-    width: small,
-  },
+  large: setSize(200),
+  medium: setSize(150),
+  small: setSize(100),
 };
 
 interface ImageProps {
   src: string;
   outline?: boolean;
-  size?: Size;
+  size?: SizeOption;
 }
-
-type ImageStyleProps = Pick<ImageProps, 'outline'> & {
-  width: CSSProperties['width'];
-  height: CSSProperties['height'];
-};
-
-const border: CSSProperties['border'] = '3px solid black';
-const StyledImage = styled.img<ImageStyleProps>(
-  ({ width, height, outline }) => {
-    return {
-      width,
-      height,
-      border: outline ? border : '',
-      borderRadius: '10px',
-    };
-  },
-);
 
 const Image = ({ src, outline = false, size = 'medium' }: ImageProps) => {
   return (
     <StyledImage src={src} alt="" outline={outline} {...imageSize[size]} />
   );
 };
+
+type ImageStyleProps = Pick<ImageProps, 'outline'> & {
+  width: CSSProperties['width'];
+  height: CSSProperties['height'];
+};
+
+const StyledImage = styled.img<ImageStyleProps>(
+  ({ width, height, outline }) => {
+    return {
+      width,
+      height,
+      border: outline ? '3px solid black' : '',
+      borderRadius: '10px',
+    };
+  },
+);
 
 export default Image;
