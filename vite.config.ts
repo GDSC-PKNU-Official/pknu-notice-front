04de1a8c59b374b1626329405d1f428559c03aa1
dohun <dohun31@naver.com>
2023-03-28 14:05:34 +0900

    Chore: @pages 절대경로 적용

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,10 @@ export default defineConfig({
         find: '@hooks',
         replacement: resolve(__dirname, './src/hooks'),
       },
+      {
+        find: '@pages',
+        replacement: resolve(__dirname, './src/pages'),
+      },
     ],
   },
 });
