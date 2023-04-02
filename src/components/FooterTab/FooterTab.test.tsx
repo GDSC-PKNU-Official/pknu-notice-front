374c6a2991adc6fafe0fbfceff5899fb0d869f44
pp449 <mma7710@naver.com>
2023-04-02 18:38:12 +0900

    Test: await을 추가해서 테스트코드 수정

--- src/components/FooterTab/FooterTab.test.tsx
+++ src/components/FooterTab/FooterTab.test.tsx
@@ -1,30 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
 
 import FooterTab from './index';
 
-describe('하단 탭 컴포넌트', () => {
-  const getIcons = (): HTMLElement[] => screen.getAllByRole('icon');
+describe('하단 탭 아이콘 클릭 테스트', () => {
+  const getIcons = () => screen.getAllByRole('button');
 
   it('클릭된 버튼을 primary 색으로 설정', () => {
-    render(<FooterTab />);
+    render(<FooterTab />, { wrapper: MemoryRouter });
     const icons = getIcons();
 
-    icons.forEach((icon) => {
-      userEvent.click(icon);
-      expect(icon).toHaveStyle({ color: '71BC5C' });
+    icons.forEach(async (icon) => {
+      await userEvent.click(icon);
+      icons.forEach((i) => {
+        const expectedColor = i === icon ? '#71BC5C' : 'black';
+        expect(i).toHaveStyle(`color: ${expectedColor}`);
+      });
     });
   });
 
   it('버튼 클릭 시 URL 이동', () => {
-    render(<FooterTab />);
+    render(<FooterTab />, { wrapper: MemoryRouter });
     const icons = getIcons();
 
     const { location } = window;
     window.location = { ...location, href: '' };
 
-    icons.forEach((icon, index) => {
-      userEvent.click(icon);
+    icons.forEach(async (icon, index) => {
+      await userEvent.click(icon);
 
       const urls = ['/map', '/', '/my'];
 
