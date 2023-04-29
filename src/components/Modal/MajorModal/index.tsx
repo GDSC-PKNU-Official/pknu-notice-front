179ddc287a7c56f04c2efc185fdd616db08c8d6c
hwinkr <dnddl8280@naver.com>
2023-04-30 00:01:13 +0900

    Feat: 학과 선택 페이지 이동 모달 창 구현

---
+++ src/components/Modal/MajorModal/index.tsx
@@ -0,0 +1,58 @@
+import Button from '@components/Button';
+import Icon from '@components/Icon';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { THEME } from '@styles/ThemeProvider/theme';
+
+import Modal from '..';
+
+interface MajorModalProps {
+  onClose: () => void;
+  routerTo: () => void;
+}
+
+const MajorModal = ({ onClose, routerTo }: MajorModalProps) => {
+  return (
+    <Modal onClose={onClose}>
+      <ModalContent>
+        <span
+          css={css`
+            color: ${THEME.TEXT.GRAY};
+            font-weight: bold;
+            margin-bottom: 15px;
+          `}
+        >
+          아직 학과를 알려주지 않았어요
+        </span>
+        <Button onClick={routerTo}>
+          <Icon kind="plus" color={THEME.TEXT.WHITE} />
+          <span>학과 선택하기</span>
+        </Button>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default MajorModal;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${THEME.TEXT.WHITE};
+  padding: 50px;
+  border-radius: 15px;
+
+  Button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+
+    flex: 0 0 15%;
+    width: 15;
+  }
+`;
