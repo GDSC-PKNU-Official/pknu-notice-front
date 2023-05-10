737a7ffaf19314af569cca8551cdee13957b4f8c
pp449 <mma7710@naver.com>
2023-05-10 17:16:36 +0900

    Feat: 단과대 선택 UI 추가

--- src/pages/MajorDecision/CollegeList/index.tsx
+++ src/pages/MajorDecision/CollegeList/index.tsx
@@ -1,5 +1,54 @@
+import http from '@apis/http';
+import Icon from '@components/Icon';
+import styled from '@emotion/styled';
+import useRouter from '@hooks/useRouter';
+import { THEME } from '@styles/ThemeProvider/theme';
+import React, { useState, useEffect } from 'react';
+
 const CollegeList = () => {
-  return <h1>안녕 !</h1>;
+  const [collegeList, setCollegeList] = useState<string[]>();
+  const { routerTo } = useRouter();
+
+  const fetchData = async () => {
+    const result = await http.get('/majorDecision');
+    setCollegeList(result.data);
+  };
+
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.target === e.currentTarget) {
+      const COLLEGENAME = e.currentTarget.textContent;
+      routerTo(`/major-decision/${COLLEGENAME}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return collegeList ? (
+    <CollegeContainer>
+      {collegeList.map((college) => (
+        <CollegeWrapper key={college} onClick={onClick}>
+          {college}
+          <IconWrapper>
+            <Icon kind="right" />
+          </IconWrapper>
+        </CollegeWrapper>
+      ))}
+    </CollegeContainer>
+  ) : (
+    <div>loading..</div>
+  );
 };
 
 export default CollegeList;
+
+const CollegeContainer = styled.div``;
+
+const CollegeWrapper = styled.div`
+  padding: 3%;
+`;
+
+const IconWrapper = styled.div`
+  float: right;
+`;
