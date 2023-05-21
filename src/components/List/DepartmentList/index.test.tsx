d54ff87373dd2bafdab42dfca3d9e53c14adfd81
pp449 <mma7710@naver.com>
2023-05-21 21:47:50 +0900

    Test: 학과선택 버튼 클릭 테스트 추가

--- src/components/List/DepartmentList/index.test.tsx
+++ src/components/List/DepartmentList/index.test.tsx
@@ -4,6 +4,23 @@ import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('학과선택 테스트', () => {
+  const oldWindowLocation = window.location;
+
+  beforeEach(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      enumerable: true,
+      value: new URL(window.location.href),
+    });
+  });
+
+  afterEach(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: false,
+      enumerable: true,
+      value: oldWindowLocation,
+    });
+  });
   it('버튼 활성화 테스트', async () => {
     await act(async () => {
       render(<DepartmentList college="정보융합대학" />, {
@@ -16,5 +33,20 @@ describe('학과선택 테스트', () => {
     const college = await screen.findByLabelText('컴퓨터인공지능학부');
     await userEvent.click(college);
     expect(confirmButton).toBeEnabled();
+  });
+
+  it('버튼 클릭 테스트', async () => {
+    await act(async () => {
+      render(<DepartmentList college="정보융합대학" />, {
+        wrapper: MemoryRouter,
+      });
+    });
+
+    const confirmButton = await screen.findByRole('button');
+    const college = await screen.findByLabelText('컴퓨터인공지능학부');
+    await userEvent.click(college);
+    await userEvent.click(confirmButton);
+
+    expect(window.location.href).toBe('https://localhost');
   });
 });
