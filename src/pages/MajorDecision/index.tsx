161f9f4e3445eaa3b71646402abf6ce0b17409bf
pp449 <mma7710@naver.com>
2023-05-22 20:51:18 +0900

    Refactor: 라우팅 설정 정상적으로 변경

--- src/pages/MajorDecision/index.tsx
+++ src/pages/MajorDecision/index.tsx
@@ -1,23 +1,7 @@
-import Button from '@components/Button';
-import useMajor from '@hooks/useMajor';
-import { useNavigate } from 'react-router-dom';
+import CollegeList from '@components/List/CollegeList';
 
 const MajorDecision = () => {
-  const navigate = useNavigate();
-  const { major, setMajor } = useMajor();
-
-  const onClick = () => {
-    setMajor('컴퓨터공학과');
-    alert('전공 선택 완료!');
-    navigate('/');
-  };
-
-  return (
-    <>
-      <Button onClick={onClick}>컴퓨터공학과로 전공 선택하기</Button>
-      <span role="note">{major}</span>
-    </>
-  );
+  return <CollegeList />;
 };
 
 export default MajorDecision;
