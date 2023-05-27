55f590d61d60757ed907492a594fa37ead78aab9
pp449 <mma7710@naver.com>
2023-05-27 20:31:49 +0900

    Refactor: onClick 타입 변경

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 
 interface ButtonProps {
   children?: ReactNode;
-  onClick?: any;
+  onClick?: React.MouseEventHandler<HTMLElement>;
   disabled?: boolean;
 }
 
