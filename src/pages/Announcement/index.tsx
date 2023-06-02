b7704aaafb2d738fcc541b8090d82a8efdcfacb1
hwinkr <dnddl8280@naver.com>
2023-06-03 00:54:28 +0900

    Feat: 전공에 따라 공지사항 보여주는 로직 추가

--- src/pages/Announcement/index.tsx
+++ src/pages/Announcement/index.tsx
@@ -1,5 +1,19 @@
-const Announcement = () => {
-  return <h1>공지사항 페이지</h1>;
-};
+import fetchAnnounceList from '@apis/Suspense/fetch-announce-list';
+import AnnounceList from '@components/Card/AnnounceCard/AnnounceList';
+import AnnounceCardSkeleton from '@components/Card/AnnounceCard/Skeleton';
+import MajorProvider from '@components/MajorProvider';
+import useMajor from '@hooks/useMajor';
+import { Suspense } from 'react';
 
+const Announcement = () => {
+  const { major } = useMajor();
+
+  return (
+    <>
+      <Suspense fallback={<AnnounceCardSkeleton length={30} />}>
+        <AnnounceList resource={fetchAnnounceList(major)} />
+      </Suspense>
+    </>
+  );
+};
 export default Announcement;
