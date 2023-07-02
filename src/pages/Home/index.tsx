09423ca6ba1881fef883d19f77947d459acfffd8
hwinkr <dnddl8280@naver.com>
2023-07-02 23:50:06 +0900

    Chore: 홈페이지에 카드 컴포넌트 렌더링 적용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,19 @@
 import InformCard from '@components/Card/InformCard';
+import { css } from '@emotion/react';
 
 const Home = () => {
   return (
-    <InformCard icon="notification" path="/announcement" title="공지사항" />
+    <>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <InformCard icon="notification" path="/announcement" title="공지사항" />
+        <InformCard icon="school" path="/announcement" title="졸업요건" />
+      </div>
+    </>
   );
 };
 
