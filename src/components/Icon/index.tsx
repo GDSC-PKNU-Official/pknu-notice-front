55955e32f75ac35ad3ef56fbe796f9619b1b1040
hwinkr <dnddl8280@naver.com>
2023-07-22 23:43:01 +0900

    Config: x 아이콘 추가

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -14,6 +14,7 @@ import {
   MdOutlineQuestionAnswer,
   MdRadioButtonUnchecked,
   MdRadioButtonChecked,
+  MdOutlineCancel,
 } from 'react-icons/md';
 
 const ICON: { [key in IconKind]: IconType } = {
@@ -30,6 +31,7 @@ const ICON: { [key in IconKind]: IconType } = {
   suggest: MdOutlineQuestionAnswer,
   checkedRadio: MdRadioButtonChecked,
   uncheckedRadio: MdRadioButtonUnchecked,
+  cancel: MdOutlineCancel,
 };
 
 interface IconProps {
