abf02e6f4d608f5d692d7aedde1cd200418ce89f
hwinkr <dnddl8280@naver.com>
2023-07-22 23:45:31 +0900

    Feat: 사용자 입력 확인 모달 구현

---
+++ src/components/Modal/ConfirmModal/index.tsx
@@ -0,0 +1,53 @@
+import Button from '@components/Button';
+import { css } from '@emotion/react';
+import { THEME } from '@styles/ThemeProvider/theme';
+import React from 'react';
+
+import Modal from '..';
+
+interface ConfirmModalProps {
+  message: string;
+  open: boolean;
+  onConfirmButtonClick: () => void;
+  onCancelButtonClick: () => void;
+}
+
+const ConfirmModal = ({
+  message,
+  open,
+  onConfirmButtonClick,
+  onCancelButtonClick,
+}: ConfirmModalProps) => {
+  return (
+    <>
+      {open && (
+        <Modal onClose={onCancelButtonClick}>
+          <>
+            <span
+              css={css`
+                color: ${THEME.TEXT.GRAY};
+                font-weight: bold;
+                margin: 0 auto;
+                margin-bottom: 15px;
+              `}
+            >
+              {message}
+            </span>
+            <div
+              css={css`
+                display: flex;
+                gap: 3.6rem;
+                justify-content: center;
+              `}
+            >
+              <Button onClick={onConfirmButtonClick}>네!</Button>
+              <Button onClick={onCancelButtonClick}>아니오</Button>
+            </div>
+          </>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default ConfirmModal;
