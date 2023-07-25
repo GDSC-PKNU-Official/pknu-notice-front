308e8be23a1953cb55517e5b7f30ba6297f5596c
hwinkr <dnddl8280@naver.com>
2023-07-25 22:11:29 +0900

    Chore: 모달 메세지 모두 upper로 변경

--- src/constants/modal-messages.ts
+++ src/constants/modal-messages.ts
@@ -1,17 +1,17 @@
 export const MODAL_MESSAGE = {
   CONFIRM: {
-    setMajor: '이 전공으로 선택할까요?',
-    editMajor: '이 전공으로 수정할까요?',
-    postSuggestion: '건의사항을 보내시겠어요?',
+    SET_MAJOR: '이 전공으로 선택할까요?',
+    EDIT_MAJOR: '이 전공으로 수정할까요?',
+    POST_SUGGESTION: '건의사항을 보내시겠어요?',
   },
   ALERT: {
-    overMapBounday: '앗! 지도의 영역을 벗어났어요',
-    overMapLevel: '앗! 더이상 지도를 축소할 수 없어요',
-    setMajor: '앗! 아직 전공을 알려주지 않았어요',
+    OVER_MAP_BOUNDARY: '앗! 지도의 영역을 벗어났어요',
+    OVER_MAP_LEVEL: '앗! 더이상 지도를 축소할 수 없어요',
+    SET_MAJOR: '앗! 아직 전공을 알려주지 않았어요',
   },
   SUCCEED: {
-    setMajor: '전공 선택 완료!',
-    editMajor: '전공 수정 완료!',
-    postSuggestion: '🙇‍♂️건의사항을 남겨 주셔서 정말 감사드립니다!🙇‍♂️',
+    SET_MAJOR: '전공 선택 완료!',
+    EDIT_MAJOR: '전공 수정 완료!',
+    POST_SUGGESTION: '🙇‍♂️건의사항을 남겨 주셔서 정말 감사드립니다!🙇‍♂️',
   },
 };
