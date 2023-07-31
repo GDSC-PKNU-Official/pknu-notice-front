af8a2019c516a2ea1d0b7cd7e821362692e55d20
Lee sang Yeop <mma7710@naver.com>
2023-07-31 15:49:39 +0900

    Style: 콘텐츠가 헤더, 푸터를 넘어가지 않도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import FooterTab from '@components/FooterTab';
 import Header from '@components/Header';
+import styled from '@emotion/styled';
 import Announcement from '@pages/Announcement';
 import Home from '@pages/Home';
 import MajorDecision from '@pages/MajorDecision';
@@ -9,18 +10,28 @@ import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <Container>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/announcement" element={<Announcement />} />
-        <Route path="/my" element={<My />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/major-decision/*" element={<MajorDecision />} />
-      </Routes>
+      <Body>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/announcement" element={<Announcement />} />
+          <Route path="/my" element={<My />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/major-decision/*" element={<MajorDecision />} />
+        </Routes>
+      </Body>
       <FooterTab />
-    </>
+    </Container>
   );
 };
 
 export default App;
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const Body = styled.div`
+  padding: 16% 0 16% 0;
+`;
