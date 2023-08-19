9106b73f8ebe1a18ebd737a2ad7e8e02cbc054df
Lee sang Yeop <mma7710@naver.com>
2023-08-19 17:46:25 +0900

    Test: MSW로 subscribe 요청 처리 추가

---
+++ src/mocks/handlers/subscribeHandler.ts
@@ -0,0 +1,8 @@
+import { SERVER_URL } from '@config/index';
+import { RequestHandler, rest } from 'msw';
+
+export const subscribeHandler: RequestHandler[] = [
+  rest.post(SERVER_URL + '/api/subscription', (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
