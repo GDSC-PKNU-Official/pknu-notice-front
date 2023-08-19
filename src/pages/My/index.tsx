34491b8aabfa10d6dfb795be7b6b817b0c15a971
Lee sang Yeop <mma7710@naver.com>
2023-08-19 17:15:12 +0900

    Style: 마이페이지 디자인 추가

--- src/pages/My/index.tsx
+++ src/pages/My/index.tsx
@@ -1,15 +1,17 @@
 import http from '@apis/http';
 import Button from '@components/Button';
+import ToggleButton from '@components/Button/Toggle';
 import Icon from '@components/Icon';
 import SuggestionModal from '@components/Modal/SuggestionModal';
 import { SERVER_URL } from '@config/index';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import urlBase64ToUint8Array from '@hooks/urlBase64ToUint8Array';
 import useMajor from '@hooks/useMajor';
 import useModals from '@hooks/useModals';
 import useRouter from '@hooks/useRouter';
 import { THEME } from '@styles/ThemeProvider/theme';
+import { MouseEventHandler, useState } from 'react';
+
 const subscribe = async () => {
   if (!('serviceWorker' in navigator)) return;
 
@@ -25,6 +27,8 @@ const subscribe = async () => {
 };
 
 const My = () => {
+  const [isToggleOn, setIsToggleOn] = useState(false);
+
   const { major } = useMajor();
   const { routerTo } = useRouter();
   const routerToMajorDecision = () => routerTo('/major-decision');
@@ -38,29 +42,30 @@ const My = () => {
     });
   };
 
+  const onClickToggle: MouseEventHandler<HTMLElement> = () => {
+    subscribe();
+    setIsToggleOn(!isToggleOn);
+  };
+
   return (
     <>
-      <h1>마이페이지</h1>
+      <Title>마이페이지</Title>
       <Major>
-        <span>전공</span>
-        <div
-          css={css`
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-          `}
-        >
-          <span>{major}</span>
-          <Icon
-            kind="edit"
-            onClick={routerToMajorDecision}
-            color={THEME.TEXT.GRAY}
-            data-testid="edit"
-          />
-        </div>
-        <Button data-testid="modal" onClick={subscribe}>
-          구독!
-        </Button>
+        <MajorCard>
+          <CardList>
+            <div>{major}</div>
+            <Icon
+              kind="edit"
+              onClick={routerToMajorDecision}
+              color={THEME.TEXT.GRAY}
+              data-testid="edit"
+            />
+          </CardList>
+          <CardList>
+            <span>공지사항 알림받기</span>
+            <ToggleButton isOn={isToggleOn} changeState={onClickToggle} />
+          </CardList>
+        </MajorCard>
       </Major>
       <Suggestion>
         <Button data-testid="modal" onClick={handleSuggestionModal}>
@@ -74,6 +79,14 @@ const My = () => {
 
 export default My;
 
+const Title = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding-top: 5%;
+  padding-left: 4%;
+  margin-bottom: 5%;
+`;
+
 const Major = styled.div`
   display: flex;
   flex-direction: column;
@@ -86,4 +99,22 @@ const Suggestion = styled.div`
   bottom: 100px;
   left: 50%;
   transform: translate(-50%, -50%);
+`;
+
+const MajorCard = styled.div`
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  width: 95%;
+  margin: 0 auto;
+  border-radius: 0.5rem;
+  align-items: center;
+  font-size: 1rem;
+`;
+
+const CardList = styled.div`
+  padding: 5%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
