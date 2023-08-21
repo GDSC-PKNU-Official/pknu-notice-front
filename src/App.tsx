282f5119e2abff0f42f502c125195a29a016a66a
hwinkr <dnddl8280@naver.com>
2023-08-21 12:03:37 +0900

    Refactor: 변경된 Body 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import FooterTab from '@components/FooterTab';
 import Header from '@components/Header';
-import styled from '@emotion/styled';
 import Announcement from '@pages/Announcement';
+import BodyLayout from '@pages/BodyLayout';
 import Home from '@pages/Home';
 import MajorDecision from '@pages/MajorDecision';
 import Map from '@pages/Map';
@@ -13,24 +13,18 @@ const App = () => {
   return (
     <>
       {location.pathname !== '/map' && <Header />}
-      {/* <Body> */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/announcement/*" element={<Announcement />} />
-        <Route path="/my" element={<My />} />
-        <Route path="/major-decision/*" element={<MajorDecision />} />
+        <Route element={<BodyLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/announcement/*" element={<Announcement />} />
+          <Route path="/major-decision/*" element={<MajorDecision />} />
+          <Route path="/my" element={<My />} />
+        </Route>
         <Route path="/map" element={<Map />} />
       </Routes>
-      {/* </Body> */}
-
       <FooterTab />
     </>
   );
 };
 
 export default App;
-
-const Body = styled.div`
-  height: calc(100vh - 16vh);
-  padding: 8vh 0 8vh 0;
-`;
