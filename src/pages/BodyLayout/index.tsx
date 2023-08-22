0f27f543fe23cfc43839789f9a132d6c0a5d39b2
hwinkr <dnddl8280@naver.com>
2023-08-22 19:20:57 +0900

    Style: 공지사항이 영역을 벗어날 경우, scroll로 처리하는 css 추가

--- src/pages/BodyLayout/index.tsx
+++ src/pages/BodyLayout/index.tsx
@@ -13,6 +13,7 @@ const BodyLayout = () => {
 export default BodyLayout;
 
 const StyledBodyLayout = styled.div`
-  height: calc(100vh - 16vh);
-  padding: 8vh 0 8vh 0;
+  height: calc(100vh - 17vh);
+  padding: 8.5vh 0 8.5vh 0;
+  overflow-y: scroll;
 `;
