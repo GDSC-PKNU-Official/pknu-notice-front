37d246ff147a485355cae8dbe10e9a01713e0eb8
hwinkr <dnddl8280@naver.com>
2023-08-27 16:31:24 +0900

    Feat: 단과대 선택 페이지에 서스펜스 적용

--- src/components/List/CollegeList/index.tsx
+++ src/components/List/CollegeList/index.tsx
@@ -1,76 +1,29 @@
-import http from '@apis/http';
-import Icon from '@components/Icon';
-import { css } from '@emotion/react';
+import fetchMajorList from '@apis/Suspense/fetch-major-list';
 import styled from '@emotion/styled';
-import useRouter from '@hooks/useRouter';
 import { THEME } from '@styles/ThemeProvider/theme';
-import React, { useState, useEffect } from 'react';
+import React, { Suspense } from 'react';
+
+import CollegeItem from './CollegeItem';
+import CollegeSkeleton from '../Skeleton/college';
 
 const CollegeList = () => {
-  const [collegeList, setCollegeList] = useState<string[]>();
-  const { routerTo } = useRouter();
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const result = await http.get('/api/majorDecision');
-    setCollegeList(result.data);
-  };
-
-  const onClick: React.MouseEventHandler<HTMLElement> = (e) => {
-    const collegeName = e.currentTarget.textContent;
-
-    if (collegeName === null) routerTo('/major-decision');
-    else routerTo(`/major-decision/${collegeName}`);
-  };
-
-  return collegeList ? (
-    <ListContainer>
+  return (
+    <>
       <Title>단과대 선택하기</Title>
-      {collegeList.map((college) => (
-        <div
-          key={college}
-          css={css`
-            width: 100%;
-          `}
-          onClick={onClick}
-        >
-          <ListWrapper>
-            {college}
-            <IconWrapper>
-              <Icon kind="right" />
-            </IconWrapper>
-          </ListWrapper>
-        </div>
-      ))}
-    </ListContainer>
-  ) : (
-    <div>loading...</div>
+      <Suspense fallback={<CollegeSkeleton length={10} />}>
+        <CollegeItem resource={fetchMajorList<string[]>()} />
+      </Suspense>
+    </>
   );
 };
 
-const ListContainer = styled.div`
-  padding-top: 2%;
-  padding-left: 2%;
-`;
-
-const ListWrapper = styled.div`
-  padding: 8% 6% 8% 6%;
-  width: 80%;
-  margin: 0 auto;
-  border-bottom: 1px solid ${THEME.BUTTON.GRAY};
-`;
-
-const IconWrapper = styled.div`
-  float: right;
-`;
-
 const Title = styled.h2`
-  font-size: 1.5rem;
-  margin-bottom: 3%;
-  padding-left: 5%;
+  font-size: 24px;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 10px;
+  color: ${THEME.TEXT.GRAY};
+  font-weight: bold;
 `;
 
 export default CollegeList;
