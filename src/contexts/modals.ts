d9ebeb08f841ea749f1ac30f6a62edd30263795f
hwinkr <dnddl8280@naver.com>
2023-09-16 17:12:20 +0900

    Refactor: 모달 state, dispatch 2개로 context 분리

--- src/contexts/modals.ts
+++ src/contexts/modals.ts
@@ -1,11 +1,17 @@
 import { Modals } from '@type/modals';
-import { createContext } from 'react';
+import { SetStateAction, createContext } from 'react';
 
-interface ModalState {
-  modals: Modals;
-  setModals: React.Dispatch<React.SetStateAction<Modals>>;
-}
+type Dispatch = React.Dispatch<SetStateAction<Modals>>;
+const ModalState = createContext<Modals | null>(null);
+const ModalDispatch = createContext<Dispatch | null>(null);
 
-const ModalsContext = createContext<ModalState | null>(null);
+const ModalContext = {
+  ModalState,
+  ModalDispatch,
+};
 
-export default ModalsContext;
+export default ModalContext;
+
+// TODO
+// 1. modal state, modal dispatch -> context 분리
+// 2. 부림이 내부에서 사용하는 modal들의 컴포넌트 상태를 정의하고 any 사용하지 않기
