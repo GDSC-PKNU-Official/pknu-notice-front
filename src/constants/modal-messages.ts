d6b7d05cd031f2d8471174f19cb273bf164cff8c
hwinkr <dnddl8280@naver.com>
2023-09-17 19:07:37 +0900

    Config: 상수로 관리하는 모달 메세지 추가

--- src/constants/modal-messages.ts
+++ src/constants/modal-messages.ts
@@ -1,17 +1,37 @@
 export const MODAL_MESSAGE = {
+  SUGGESTION_TITLE: '건의사항',
   CONFIRM: {
     SET_MAJOR: '이 전공으로 선택할까요?',
     EDIT_MAJOR: '이 전공으로 수정할까요?',
     POST_SUGGESTION: '건의사항을 보내시겠어요?',
+    ALARM: '알림을 그만 받을까요?',
   },
   ALERT: {
     OVER_MAP_BOUNDARY: '앗! 지도의 영역을 벗어났어요',
     OVER_MAP_LEVEL: '앗! 더이상 지도를 축소할 수 없어요',
     SET_MAJOR: '앗! 아직 전공을 알려주지 않았어요',
+    GET_LOCATION: '위치정보를 가져오는 중입니다...',
+    NO_LOCATION_PERMISSON:
+      '위치정보를 제공하지 않아 \n길찾기 기능을 사용할 수 없어요!',
+    OUTSIDE_SCHOOL:
+      '학교 외부에 있어요!\n 길찾기 기능을 이용해주세요.\n 현재주소',
+    NO_SEARCH_KEYWORD: '검색어를 입력해주세요!',
+    SEARCH_FAILED:
+      '찾으시는 건물이 존재하지 않아요!\n 검색어를 다시 확인해주세요.',
   },
   SUCCEED: {
     SET_MAJOR: '전공 선택 완료!',
     EDIT_MAJOR: '전공 수정 완료!',
-    POST_SUGGESTION: '🙇‍♂️건의사항을 남겨 주셔서 정말 감사드립니다!🙇‍♂️',
+    POST_SUGGESTION: '건의사항을 남겨 주셔서 정말 감사드립니다!',
   },
+};
+
+export const MODAL_BUTTON_MESSAGE = {
+  YES: '네!',
+  NO: '아니오',
+  CONFIRM: '확인',
+  CLOSE: '닫기',
+  GO_HOME: '홈으로 이동하기',
+  SET_MAJOR: '전공선택하러 가기',
+  SEND_SUGGESTION: '보내기',
 };
