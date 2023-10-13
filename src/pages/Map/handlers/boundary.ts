5b5c824abb388a4b9ca5ca6a5f35aa2d35dd1de4
hwinkr <dnddl8280@naver.com>
2023-10-13 10:32:46 +0900

    Refactor: 이동시킬 방향을 결정하는 로직을 함수로 분리

--- src/pages/Map/handlers/boundary.ts
+++ src/pages/Map/handlers/boundary.ts
@@ -4,31 +4,31 @@ import {
   RESET_COORDINATES,
 } from '@constants/pknu-map';
 
-const handleMapBoundary = (map: any) => {
-  if (!map) {
-    return null;
-  }
+const gerOverDirection = (La: number, Ma: number): DIRECTION | undefined => {
+  if (Ma >= PKNU_MAP_LIMIT.TOP) return 'TOP';
+  if (La >= PKNU_MAP_LIMIT.RIGHT) return 'RIGHT';
+  if (Ma <= PKNU_MAP_LIMIT.BOTTOM) return 'BOTTOM';
+  if (La <= PKNU_MAP_LIMIT.LEFT) return 'LEFT';
+  return;
+};
+const getResetDirection = (overDirection: DIRECTION) => {
+  return new window.kakao.maps.LatLng(
+    RESET_COORDINATES[overDirection].lat,
+    RESET_COORDINATES[overDirection].lng,
+  );
+};
 
-  const gerOverDirection = (La: number, Ma: number): DIRECTION | undefined => {
-    if (Ma >= PKNU_MAP_LIMIT.TOP) return 'TOP';
-    if (La >= PKNU_MAP_LIMIT.RIGHT) return 'RIGHT';
-    if (Ma <= PKNU_MAP_LIMIT.BOTTOM) return 'BOTTOM';
-    if (La <= PKNU_MAP_LIMIT.LEFT) return 'LEFT';
-    return;
-  };
+const handleMapBoundary = (map: any) => {
+  if (!map) return null;
 
   const handleBoundaryLimit = () => {
     const { La, Ma } = map.getCenter();
-
     const overDirection = gerOverDirection(La, Ma);
-    if (!overDirection) return;
 
+    if (!overDirection) return;
     map.setDraggable(false);
     setTimeout(() => {
-      const resetDirection = new window.kakao.maps.LatLng(
-        RESET_COORDINATES[overDirection].lat,
-        RESET_COORDINATES[overDirection].lng,
-      );
+      const resetDirection = getResetDirection(overDirection);
       map.panTo(resetDirection);
       map.setDraggable(true);
     }, 300);
