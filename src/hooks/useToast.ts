272aedd98250099066c09311a4bcc1c2005ea63d
hwinkr <dnddl8280@naver.com>
2023-10-16 19:04:26 +0900

    Chore: addToast 함수에서 icon 인자 삭제

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,5 +1,4 @@
 import ToastContext from '@contexts/toasts';
-import { IconKind } from '@type/styles/icon';
 import { useContext } from 'react';
 
 const useToasts = () => {
@@ -14,12 +13,11 @@ const useToasts = () => {
       prevToasts.filter((prevToast) => prevToast.id !== id),
     );
   };
-  const addToast = (message: string, icon: IconKind) => {
+  const addToast = (message: string) => {
     const randomId = Math.random().toString(36).substring(2, 16);
     const newToast = {
       id: randomId,
       message,
-      icon,
     };
     setToasts((prevToasts) => [...prevToasts, newToast]);
   };
