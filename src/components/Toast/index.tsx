c9e4cb71ccc08c4d7dc0a9a3233d61fa160a12bd
hwinkr <dnddl8280@naver.com>
2023-10-16 19:05:48 +0900

    Chore: Toast 컴포넌트에서 icon, progressBar 삭제

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,18 +1,15 @@
-import Icon from '@components/Icon';
 import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import useToasts from '@hooks/useToast';
 import { THEME } from '@styles/ThemeProvider/theme';
-import { IconKind } from '@type/styles/icon';
 import React, { useEffect, useState } from 'react';
 
 interface ToastProps {
   id: string;
-  icon: IconKind;
   message: string;
 }
 
-const Toast = ({ id, icon, message }: ToastProps) => {
+const Toast = ({ id, message }: ToastProps) => {
   const [isClose, setIsClose] = useState<boolean>(false);
   const { removeToast } = useToasts();
 
@@ -38,9 +35,7 @@ const Toast = ({ id, icon, message }: ToastProps) => {
 
   return (
     <Container isClose={isClose}>
-      <Icon kind={icon} size="18" color={THEME.PRIMARY} />
       <ToastMessage>{message}</ToastMessage>
-      <ProgressBar></ProgressBar>
     </Container>
   );
 };
@@ -50,32 +45,29 @@ export default Toast;
 const toastSlideIn = keyframes`
   from{
     opacity: 0;
-    transform: translateX(100%);
   }to{
     opacity: 1;
-    transform: translateX(0%);
   }
 `;
 
 const toastSlideOut = keyframes`
   from{
     opacity: 1;
-    transform: translateX(0%);
   }to{
     opacity: 0;
-    transform: translateX(100%);
   }
 `;
 
 const Container = styled.div<{ isClose: boolean }>`
-  width: 60%;
   position: relative;
   padding: 0.8rem;
   gap: 1rem;
 
   display: flex;
+  justify-content: center;
   align-items: center;
-  background-color: ${THEME.TEXT.WHITE};
+  border-radius: 25px;
+  background-color: ${THEME.PRIMARY};
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.4);
   animation: ${({ isClose }) => (isClose ? toastSlideOut : toastSlideIn)} 0.4s
     ease-in-out forwards;
@@ -84,23 +76,7 @@ const Container = styled.div<{ isClose: boolean }>`
 
 const ToastMessage = styled.span`
   font-size: 12px;
-`;
-
-const progressBar = keyframes`
-    from{
-        width: 100%;
-    }to{
-        width: 0%;
-    }
-`;
-const ProgressBar = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-
-  width: 100%;
-  height: 4px;
-  background-color: ${THEME.PRIMARY};
-
-  animation: ${progressBar} 2s linear forwards;
+  font-weight: 500;
+  line-height: normal;
+  color: ${THEME.TEXT.WHITE};
 `;
