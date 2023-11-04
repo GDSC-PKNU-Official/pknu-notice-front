875de9e7de876fe98ceef0265bf9909cc70432eb
hwinkr <dnddl8280@naver.com>
2023-11-04 16:00:30 +0900

    Config: 공지사항 관련 타입 추가
    - AnnouncementCategory : 'shool', 'major'와 같이 공지사항 분류 타입을 설정
    - AnnouncementType : '일반', '고정' 처럼 확인 가능한 공지사항의 분류 타입을 설정

--- src/@types/announcement.ts
+++ src/@types/announcement.ts
@@ -1,3 +1,8 @@
+import {
+  ANNOUNCEMENT_CATEGORY,
+  ANNOUNCEMENT_TYPE,
+} from '@constants/announcement';
+
 type AnnounceItemType = '고정' | '일반';
 
 export interface AnnounceItem {
@@ -11,3 +16,9 @@ export interface AnnounceItem {
 export type AnnounceItemList = {
   [key in AnnounceItemType]: AnnounceItem[];
 };
+
+export type AnnouncementCategory =
+  (typeof ANNOUNCEMENT_CATEGORY)[keyof typeof ANNOUNCEMENT_CATEGORY];
+
+export type AnnouncementType =
+  (typeof ANNOUNCEMENT_TYPE)[keyof typeof ANNOUNCEMENT_TYPE];
