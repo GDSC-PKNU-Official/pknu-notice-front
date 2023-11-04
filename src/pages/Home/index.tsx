bec98e356e879c63bed5c542fa06eea64a5f3643
hwinkr <dnddl8280@naver.com>
2023-11-04 16:25:38 +0900

    Refactor: 홈페이지 컴포넌트 구성 변경
    - InformCardList: 공지사항 관련 카드 컴포넌트를 추상화
    - 홈페이지 컴포넌트에서 담당하던 graduationLink의 상태 관리를 MajorPovider로 이전

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,49 +1,15 @@
-import http from '@apis/http';
-import InformCard from '@components/Card/InformCard';
 import Carousel from '@components/Carousel';
 import styled from '@emotion/styled';
-import useMajor from '@hooks/useMajor';
-import useRouter from '@hooks/useRouter';
 import { THEME } from '@styles/ThemeProvider/theme';
-import { AxiosResponse } from 'axios';
-import { useEffect, useState } from 'react';
+
+import InformCardList from './components/InformCardList';
 
 const Home = () => {
-  const [graduationLink, setGraduationLink] = useState<string | null>('');
-  const { routerTo } = useRouter();
-  const { major } = useMajor();
-
-  const routerToGraduationRequiredPage = (graduationLink: string | null) => {
-    graduationLink && window.open(graduationLink, '_blank');
-  };
-
-  useEffect(() => {
-    if (!major) return;
-    const getGraduationLink = async () => {
-      const response: AxiosResponse<GraduationLink> = await http.get(
-        `/api/graduation?major=${major}`,
-      );
-      setGraduationLink(response.data.link);
-    };
-    getGraduationLink();
-  }, [major]);
-
   return (
     <Container>
       <InformCardWrapper>
         <InformTitle>학교</InformTitle>
-        <InformCard
-          icon="notification"
-          title="공지사항"
-          majorRequired={false}
-          onClick={() => routerTo('/announcement')}
-        />
-        <InformCard
-          icon="school"
-          title="졸업요건"
-          majorRequired={true}
-          onClick={() => routerToGraduationRequiredPage(graduationLink)}
-        />
+        <InformCardList />
       </InformCardWrapper>
       <InformCardWrapper>
         <InformTitle>비교과</InformTitle>
@@ -76,8 +42,3 @@ const InformTitle = styled.div`
   font-size: 20px;
   font-weight: bold;
 `;
-
-interface GraduationLink {
-  department: string;
-  link: string | null;
-}
