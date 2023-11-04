5b987fe12d19cdc7aa6a247114fe3ae76e3f03c0
hwinkr <dnddl8280@naver.com>
2023-11-04 16:14:45 +0900

    Chore: placeholder에 공지사항 검색 추가

--- src/constants/placeholder-message.ts
+++ src/constants/placeholder-message.ts
@@ -1,6 +1,7 @@
 const PLCACEHOLDER_MESSAGES = {
   SUGGESTION: '건의사항을 5글자 이상 남겨주세요',
   SEARCH_BUILDING: '건물번호 또는 건물이름을 검색해주세요',
+  SEARCH_TITLE: '제목을 검색하세요',
 } as const;
 
 export default PLCACEHOLDER_MESSAGES;
