0737cb073b3f323ab5f4bc037700379e46db54aa
hwinkr <dnddl8280@naver.com>
2023-11-04 16:13:29 +0900

    Config: 공지사항 관련 상수 설정
    - TITLE: 공지사항 제목
    - CATEGORY: 공지사항 분류, 어학 취업정보가 추가될 경우 여기에 추가할 수 있도록 함
    - TYPE: API를 통해 가져온 공지사항을 분류

---
+++ src/constants/announcement.ts
@@ -0,0 +1,15 @@
+export const ANNOUNCEMENT_TITLE = {
+  SCHOOL: '학교 공지사항',
+  MAROR: '학과 공지사항',
+};
+
+export const ANNOUNCEMENT_CATEGORY = {
+  SCHOOL: 'school',
+  MAJOR: 'major',
+} as const;
+
+export const ANNOUNCEMENT_TYPE = {
+  NORMAL: 'normal',
+  PINNED: 'pinned',
+  SEARCH: 'search',
+} as const;
