394655b5278c1fa5439bcc23c52f6a1b060f7177
hwinkr <dnddl8280@naver.com>
2023-11-04 16:15:39 +0900

    Refactor: MajorContext에 graduationLink 추가

--- src/contexts/major.ts
+++ src/contexts/major.ts
@@ -4,6 +4,7 @@ import { createContext } from 'react';
 interface MajorState {
   major: Major;
   setMajor: React.Dispatch<React.SetStateAction<Major>>;
+  graduationLink: string | null;
 }
 
 const MajorContext = createContext<MajorState | null>(null);
