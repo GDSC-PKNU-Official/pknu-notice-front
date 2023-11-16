47b3c3bbd0f5046f58acd8f7201c393abb1b5bd5
hwinkr <dnddl8280@naver.com>
2023-11-17 00:16:59 +0900

    Refactor: 공지사항 검색 결과를 판단하는 함수 로직 수정

--- src/pages/Announcement/components/AnnounceSearchList.tsx
+++ src/pages/Announcement/components/AnnounceSearchList.tsx
@@ -1,10 +1,9 @@
 import AnnounceCard from '@components/Card/AnnounceCard';
-import Icon from '@components/Icon';
-import styled from '@emotion/styled';
-import { THEME } from '@styles/ThemeProvider/theme';
 import { AnnounceItem } from '@type/announcement';
 import React, { Fragment } from 'react';
 import { useLocation } from 'react-router-dom';
+
+import AnnounceNotFound from './AnnounceNotFound';
 
 interface AnnounceSearchProps {
   announceList: AnnounceItem[];
@@ -17,7 +16,7 @@ const AnnounceSearchList = ({ announceList }: AnnounceSearchProps) => {
   const searchResult = announceList.filter((announceItem) =>
     announceItem.title.includes(searchKeyword),
   );
-  const hasSearchResult = () => searchResult.length === 0;
+  const hasSearchResult = () => searchResult.length !== 0;
 
   return (
     <Fragment>
@@ -28,28 +27,10 @@ const AnnounceSearchList = ({ announceList }: AnnounceSearchProps) => {
           </Fragment>
         ))
       ) : (
-        <AnnounceNotFound>
-          <Icon kind="warning" color={THEME.TEXT.GRAY} />
-          <NotFoundTitle>
-            {`"${searchKeyword}"`}에 관한 공지사항이 없습니다.
-          </NotFoundTitle>
-        </AnnounceNotFound>
+        <AnnounceNotFound keyword={searchKeyword} />
       )}
     </Fragment>
   );
 };
 
 export default AnnounceSearchList;
-
-const AnnounceNotFound = styled.div`
-  height: 20rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-`;
-
-const NotFoundTitle = styled.span`
-  color: ${THEME.TEXT.GRAY};
-`;
