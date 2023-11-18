f5af4a4f82157ec019addc129c07714a161c7012
hwinkr <dnddl8280@naver.com>
2023-11-18 17:19:13 +0900

    feat(add component): FAQ 컴포넌트 구현
    - 질문, 응답으로 구성
    - 응답에 대한 렌더링 상태를 showAnswer을 통해서 관리
    - 링크가 있는 경우에만 링크를 렌더링

---
+++ src/components/FAQBox/index.tsx
@@ -0,0 +1,107 @@
+import Icon from '@components/Common/Icon';
+import { FQA_CONSTANTS } from '@constants/FAQ';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { THEME } from '@styles/ThemeProvider/theme';
+import openLink from '@utils/router/openLink';
+import React, { useState } from 'react';
+
+interface FAQBoxProps {
+  readonly question: string;
+  readonly answer: {
+    readonly text: string;
+    readonly link?: string;
+  };
+}
+
+const FAQBox = ({ question, answer }: FAQBoxProps) => {
+  const [showAnswer, setShowAnswer] = useState<boolean>(false);
+  const toggleAnswer = () => setShowAnswer((prevState) => !prevState);
+
+  const moveToLink = () => {
+    if (!answer.link) return;
+    openLink(answer.link);
+  };
+
+  const hasAnswerLink = () => !!answer.link;
+  const answerTextSeperatedLine = answer.text.split(
+    FQA_CONSTANTS.LINE_SEPERATOR,
+  );
+
+  return (
+    <>
+      <QuestionContainer onClick={toggleAnswer} showAnswer={showAnswer}>
+        <QuestionMark>{FQA_CONSTANTS.QUESTION_MARK}</QuestionMark>
+        <QuestionText>{question}</QuestionText>
+        <IconContainer>
+          <Icon kind="arrowRight" size="24" />
+        </IconContainer>
+      </QuestionContainer>
+      {showAnswer && (
+        <AnswerContainer>
+          {answerTextSeperatedLine.map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+          {hasAnswerLink() && (
+            <StyledLink onClick={moveToLink}>{FQA_CONSTANTS.LINK}</StyledLink>
+          )}
+        </AnswerContainer>
+      )}
+
+      <BoundaryLine />
+    </>
+  );
+};
+
+export default FAQBox;
+
+const QuestionContainer = styled.div<{ showAnswer: boolean }>`
+  position: relative;
+  padding: 10px 0px 10px 0px;
+  display: flex;
+  align-items: center;
+
+  ${({ showAnswer }) => css`
+    & > span {
+      color: ${showAnswer && THEME.PRIMARY};
+    }
+    & > div > svg {
+      transform: ${showAnswer ? 'rotate(90deg)' : 'rotate(0deg)'};
+      transition: all ease 0.3s;
+    }
+  `}
+`;
+
+const QuestionMark = styled.span`
+  font-weight: bold;
+`;
+
+const QuestionText = styled.span`
+  text-indent: 1rem;
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const AnswerContainer = styled.div`
+  background-color: #7a9dd31a;
+  color: ${THEME.TEXT.BLACK};
+  line-height: 1.8;
+  padding: 10px 20px 10px 20px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+`;
+
+const StyledLink = styled.span`
+  color: ${THEME.PRIMARY};
+  border-bottom: 1px solid ${THEME.PRIMARY};
+`;
+
+const BoundaryLine = styled.div`
+  border-bottom: 1px solid #ededed;
+`;
