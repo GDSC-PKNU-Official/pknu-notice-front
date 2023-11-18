e75beff80169d8796a22114d4c7cd0fdcdb41ff2
hwinkr <dnddl8280@naver.com>
2023-11-18 17:19:57 +0900

    config(add constants): FAQ 페이지 구현에 사용되는 상수 설정
    - 질문, 응답 데이터
    - 문자열 상수

---
+++ src/constants/FAQ/faq-constants.ts
@@ -0,0 +1,9 @@
+const FQA_CONSTANTS = {
+  QUESTION_MARK: 'Q.',
+  LINE_SEPERATOR: '\n',
+  TITLE: '자주 묻는 질문',
+  SUB_TITLE: '부림이 서비스 관련 문의사항은 언제든지 건의사항에 남겨주세요!',
+  LINK: '링크',
+} as const;
+
+export default FQA_CONSTANTS;
