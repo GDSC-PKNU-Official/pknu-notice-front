ce845c11cd9c428abaf28d6f0092843c67849112
hwinkr <dnddl8280@naver.com>
2023-11-27 16:14:26 +0900

    config(modify tip constants): 꿀팁 페이지에서 사용할 데이터 및 상수 설정

--- src/constants/tip.ts
+++ src/constants/tip.ts
@@ -1,52 +1,105 @@
-import { IconKind } from '@type/styles/icon';
-
-import ASSET_PATH from './assets-path';
-
-interface SideBarContent {
-  readonly id: number;
-  readonly title: string;
-  readonly path: string;
-  readonly iconKind?: IconKind;
-  readonly imagePath?: string;
+export interface TipData {
+  title: string;
+  subTitle: string;
+  webpPath: string;
+  pngPath: string;
+  link: string;
 }
 
-const TIP_CONTENT: SideBarContent[] = [
+export const SHORTCUT_DATA: TipData[] = [
   {
-    id: 1,
     title: '부경대학교',
-    path: 'https://www.pknu.ac.kr/main',
-    imagePath: `${ASSET_PATH.PKNU}`,
+    subTitle: '부경대학교\n홈페이지로 이동',
+    webpPath: '/assets/tipImages/webp/pknu.webp',
+    pngPath: '/assets/tipImages/png/pknu.png',
+    link: 'https://www.pknu.ac.kr/main',
   },
   {
-    id: 2,
     title: '웨일비',
-    path: 'https://whalebe.pknu.ac.kr/main/1',
-    imagePath: `${ASSET_PATH.WHALEBE}`,
+    subTitle: '웨일비 비교과\n홈페이지로 이동',
+    webpPath: '/assets/tipImages/webp/baekgyeong_love.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_love.png',
+    link: 'https://whalebe.pknu.ac.kr/main',
   },
   {
-    id: 3,
-    title: '진로취업길라잡이',
-    path: 'https://pknujob.pknu.ac.kr/main',
-    iconKind: 'handshake',
+    title: '진로취업\n길라잡이',
+    subTitle: '취업관련\n정보, 검사 하러가기',
+    webpPath: '/assets/tipImages/webp/baekgyeong_teach.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_teach.png',
+    link: 'https://pknujob.pknu.ac.kr/main',
   },
   {
-    id: 4,
-    title: '증명서발급센터',
-    path: 'https://pknu.certpia.com/',
-    iconKind: 'print',
+    title: '증명서\n발급센터',
+    subTitle: '각종 증명서\n발급하러 가기',
+    webpPath: '/assets/tipImages/webp/baekgyeong_search.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_search.png',
+    link: 'https://pknu.certpia.com/',
   },
   {
-    id: 5,
-    title: '강의계획서 조회',
-    path: 'https://irumi.pknu.ac.kr/nxui/launch.html?screenid=Desktop_screen_quick&menuId=U020913',
-    iconKind: 'document',
+    title: '이루미',
+    subTitle: '부경대 포털 사이트\n바로가기',
+    webpPath: '/assets/tipImages/webp/baekgyeong_teach.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_teach.png',
+    link: 'https://portal.pknu.ac.kr/',
   },
   {
-    id: 6,
-    title: '아르바이트',
-    path: 'https://www.pknu.ac.kr/main/145',
-    iconKind: 'personSearch',
+    title: '도서관',
+    subTitle: '도서관 정보\n확인하기',
+    webpPath: '/assets/tipImages/webp/baekgyeong_camera.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_camera.png',
+    link: 'https://libweb.pknu.ac.kr/',
+  },
+  {
+    title: '해커스',
+    subTitle: '토익, 스피킹 등\n외국어 무료 강좌',
+    webpPath: '/assets/tipImages/webp/baekgyeong_hi.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_hi.png',
+    link: 'https://pknulib.champstudy.com/',
   },
 ];
 
-export default TIP_CONTENT;
+export const HONEY_TIP_DATA: TipData[] = [
+  {
+    title: '아우란트검사',
+    subTitle: '인성·역량·취업준비도\n·진로적성검사',
+    webpPath: '/assets/tipImages/webp/baekgyeong_hi.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_hi.png',
+    link: 'https://u.educe.co.kr/pknu',
+  },
+  {
+    title: '동아리/스터디',
+    subTitle: '동아리/스터디\n모집 정보 알아보기',
+    webpPath: '/assets/tipImages/webp/baekgyeong_love.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_love.png',
+    link: 'https://www.pknu.ac.kr/main/143',
+  },
+  {
+    title: '아르바이트',
+    subTitle: '알바 정보 보러가기',
+    webpPath: '/assets/tipImages/webp/baekgyeong_camera.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_camera.png',
+    link: 'https://www.pknu.ac.kr/main/145',
+  },
+  {
+    title: '강의계획서\n조회',
+    subTitle: '강의정보 열람하기',
+    webpPath: '/assets/tipImages/webp/baekgyeong_teach.webp',
+    pngPath: '/assets/tipImages/png/baekgyeong_teach.png',
+    link: 'https://irumi.pknu.ac.kr/nxui/launch.html?screenid=Desktop_screen_quick&menuId=U020913',
+  },
+];
+
+export const TIP_TYPE = {
+  SHORTCUT: 'shortcut',
+  HONEY_TIP: 'honeytip',
+};
+
+export const TIP_PAGE = {
+  TITLE: '꿀팁 사이트',
+  SUB_TITLE:
+    '알아두면 쓸모 있는 학교 관련 정보 페이지들입니다.\n카드를 누르면 바로 사이트로 이동해요.',
+  BUTTON: {
+    SHORTCUT: '바로가기',
+    HONEY_TIP: '꿀팁',
+  },
+} as const;
