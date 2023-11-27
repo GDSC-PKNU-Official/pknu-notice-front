34806d6de39a9426c90fb799f29a0c4c10139a5b
hwinkr <dnddl8280@naver.com>
2023-11-27 16:09:00 +0900

    feat(util function): 정보 페이지 상단 레이아웃 합성 컴포넌트에서 렌더링 할 자식 컴포넌트를 결정하는 함수 구현
    - 팁 카드 합성 컴포넌트에서 사용하는 함수와 구현이 동일

---
+++ src/components/InformUpperLayout/domain/getInformUpperLayoutSubElement.ts
@@ -0,0 +1,24 @@
+import { Children, isValidElement } from 'react';
+
+import InformSubTitle from '../InformSubTitle';
+import InformTitle from '../InformTitle';
+import InformTypeButton from '../InformTypeButton';
+
+type InformUpperLayoutChildType =
+  | typeof InformTitle
+  | typeof InformSubTitle
+  | typeof InformTypeButton;
+
+const getInformUpperLayoutSubElement = (
+  children: React.ReactNode,
+  childType: InformUpperLayoutChildType,
+) => {
+  const childrenArray = Children.toArray(children);
+  const targetChild = childrenArray
+    .filter((child) => isValidElement(child) && child.type === childType)
+    .slice(0, 2);
+
+  return targetChild;
+};
+
+export default getInformUpperLayoutSubElement;
