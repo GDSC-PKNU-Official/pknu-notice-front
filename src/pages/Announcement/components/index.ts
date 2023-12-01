4a4b806262b9873179a052394281dfbc4c5ae9ae
hwinkr <dnddl8280@naver.com>
2023-12-01 13:09:21 +0900

    chore(delete unused component): 사용하지 않는 컴포넌트 export 경로에서 제거

--- src/pages/Announcement/components/index.ts
+++ src/pages/Announcement/components/index.ts
@@ -1,4 +1,2 @@
 export { default as AnnounceContainer } from './AnnounceContainer';
-export { default as AnnounceSearch } from './AnnounceSearch';
 export { default as AnnounceSearchList } from './AnnounceSearchList';
-export { default as AnnounceTypeButtons } from './AnnounceTypeButtons';
