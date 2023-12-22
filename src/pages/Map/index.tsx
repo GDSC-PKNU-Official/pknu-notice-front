e9dd4cd3cbebc624f2341bb26b09f759d3f750d0
hwinkr <dnddl8280@naver.com>
2023-12-22 22:47:49 +0900

    refactor(MapPage): context api와 합성 컴포넌트를 사용해서 의존성 주입을 하도록 수정
    - Map(Provider) 컴포넌트만 의존해서 렌더링할 수 있음
    - Provider에서 의존성을 주입하고, 커스텀 훅으로 로직을 꺼내서 사용하기 때문에 의존성 주입을 외부에 드러내지 않아도 됨

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,12 +1,4 @@
-import { PKNU_MAP_CENTER_LOCATION } from '@constants/pknu-map';
-import styled from '@emotion/styled';
-import useLocationHandler from '@hooks/useLocationHandler';
-import useModals from '@hooks/useModals';
-import useUserLocation from '@hooks/useUserLocation';
-import { useEffect, useState } from 'react';
-
-import { FilterButtons, MapHeader, RefreshButtons } from './components';
-import handleMapBoundary from './handlers/boundary';
+import Map from '@components/Providers/MapProvider';
 
 declare global {
   interface Window {
@@ -14,46 +6,15 @@ declare global {
   }
 }
 
-const Map = () => {
-  const [map, setMap] = useState(null);
-  const { openModal, closeModal } = useModals();
-  const userLocation = useUserLocation();
-
-  useEffect(() => {
-    const container = document.getElementById('map');
-    const options = {
-      center: PKNU_MAP_CENTER_LOCATION,
-      level: 4,
-      minLevel: 1,
-      maxLevel: 4,
-    };
-    const map = new window.kakao.maps.Map(container as HTMLDivElement, options);
-    setMap(map);
-  }, []);
-
-  useLocationHandler(map, openModal, closeModal);
-  handleMapBoundary(map);
-
+const MapPage = () => {
   return (
-    <MapContainer>
-      <MapHeader map={map} />
-      <FilterButtons map={map} userLocation={userLocation} />
-      <RefreshButtons map={map} userLocation={userLocation} />
-      <KakaoMap id="map" />
-    </MapContainer>
+    <Map>
+      <Map.PknuMap />
+      <Map.MapHeader />
+      <Map.FilterButtons />
+      <Map.RefreshButtons />
+    </Map>
   );
 };
 
-export default Map;
-
-const MapContainer = styled.section`
-  height: calc(100vh - 8vh);
-  display: flex;
-  flex-direction: column;
-  position: relative;
-`;
-
-const KakaoMap = styled.div`
-  height: calc(100vh - 90px);
-  width: 100%;
-`;
+export default MapPage;
