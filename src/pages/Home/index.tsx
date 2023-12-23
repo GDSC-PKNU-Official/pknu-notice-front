798e03be657c07e5ab8f5177555975b7f55ecff9
Lee sang Yeop <mma7710@naver.com>
2023-12-23 21:34:12 +0900

    feat(pages/Home): 홈 화면에 HalfCard 컴포넌트 추가 해당 컴포넌트에 취업 길라잡이, 어학 공지사항 카드 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import Carousel from '@components/Carousel';
 import styled from '@emotion/styled';
+import InformHalfCardList from '@pages/Home/components/InformHalfCardList';
 import { THEME } from '@styles/ThemeProvider/theme';
 
 import InformCardList from './components/InformCardList';
@@ -11,6 +12,20 @@ const Home = () => {
         <InformTitle>학교</InformTitle>
         <InformCardList />
       </InformCardWrapper>
+      <HalfCardWrapper>
+        <InformHalfCardList
+          iconKind="account"
+          title="취업 길라잡이"
+          subTitle="채용 정보 확인"
+          link="/announcement/recruit/normal"
+        />
+        <InformHalfCardList
+          iconKind="language"
+          title="어학 공지사항"
+          subTitle="어학 정보 확인"
+          link="/announcement/language/normal"
+        />
+      </HalfCardWrapper>
       <InformCardWrapper>
         <InformTitle>비교과</InformTitle>
         <Carousel />
@@ -36,6 +51,12 @@ const InformCardWrapper = styled.div`
   background-color: ${THEME.IVORY};
   margin-top: 5%;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+
+const HalfCardWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5%;
 `;
 
 const InformTitle = styled.div`
