eb89fdb7281c7545ab1976134ecf4aca9a554d52
hwinkr <dnddl8280@naver.com>
2023-12-24 18:55:26 +0900

    chore(change method): 건물 검색 키워드를 포매팅할 때, replaceAll 메서드를 사용하는 것으로 변경

--- src/utils/map/get-building-info.ts
+++ src/utils/map/get-building-info.ts
@@ -4,15 +4,15 @@ import { BuildingType } from '@type/map';
 const getBuildingInfo = (
   keyword: string,
 ): [BuildingType, number] | undefined => {
-  const splittedKeyword = keyword.split(' ').join('').toUpperCase();
+  const formattedKeyword = keyword.replaceAll(' ', '').toUpperCase();
 
   for (const buildingType of Object.keys(PKNU_BUILDINGS)) {
     const index = PKNU_BUILDINGS[
       buildingType as BuildingType
     ].buildings.findIndex(
       (PKNU_BUILDING) =>
-        PKNU_BUILDING.buildingName === splittedKeyword ||
-        PKNU_BUILDING.buildingNumber === splittedKeyword,
+        PKNU_BUILDING.buildingName === formattedKeyword ||
+        PKNU_BUILDING.buildingNumber === formattedKeyword,
     );
     if (index !== -1) return [buildingType as BuildingType, index];
   }
