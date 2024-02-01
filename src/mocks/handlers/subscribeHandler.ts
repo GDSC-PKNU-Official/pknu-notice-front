b7b7a556460aba1ca3e09bc797ed9541ab7cc4a7
Lee sang Yeop <mma7710@naver.com>
2024-02-02 03:17:31 +0900

    test(subscribeHandler): 키워드 관련 API 모킹 구독한 키워드 조회, 신규 키워드 구독, 키워드 삭제 요청 모킹

--- src/mocks/handlers/subscribeHandler.ts
+++ src/mocks/handlers/subscribeHandler.ts
@@ -8,4 +8,15 @@ export const subscribeHandler: RequestHandler[] = [
   rest.delete(SERVER_URL + '/api/subscription', (req, res, ctx) => {
     return res(ctx.status(200));
   }),
+  rest.get(SERVER_URL + '/api/subscription/keyword', (req, res, ctx) => {
+    const mockData = ['장학', '등록', '네이버'];
+
+    return res(ctx.status(200), ctx.json(mockData));
+  }),
+  rest.post(SERVER_URL + '/api/subscription/keyword', (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+  rest.delete(SERVER_URL + '/api/subscription/keyword', (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
 ];
