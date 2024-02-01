93fe6e7e2d16fda94a5862004f195f1c7d9c9837
Lee sang Yeop <mma7710@naver.com>
2024-02-02 03:18:23 +0900

    feat(constants keyword): 키워드 페이지에 필요한 문구를 상수로 정의

---
+++ src/constants/keyword.ts
@@ -0,0 +1,7 @@
+export const KEYWORD_PAGE = {
+  TITLE: '키워드 알림 설정',
+  SUB_TITLE:
+    '키워드를 설정하시면 키워드가 포함된 공지사항이 올라올 때마다 알림을 보내드려요.',
+  PLACEHOLDER: '키워드를 입력해주세요. (예 : 장학금)',
+  REGISTERED_KEYWORD: '등록한 키워드',
+};
