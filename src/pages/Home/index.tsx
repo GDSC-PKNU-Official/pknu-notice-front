7877e799734a10cc3a6d646025c4cc1ce3dc3259
hwinkr <dnddl8280@naver.com>
2024-02-05 16:20:37 +0900

    Refactor(Home): 홈페이지 구성 컴포넌트 추상화

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,35 +1,15 @@
-import Carousel from '@components/Carousel';
 import styled from '@emotion/styled';
-import InformHalfCard from '@pages/Home/components/InformHalfCard';
-import { THEME } from '@styles/ThemeProvider/theme';
 
-import InformCardList from './components/InformCardList';
+import ExtraInfo from './components/ExtraInfo';
+import SchoolInfo from './components/SchoolInfo';
+import Whalbe from './components/Whalbe';
 
 const Home = () => {
   return (
     <Container>
-      <InformCardWrapper>
-        <InformTitle>학교</InformTitle>
-        <InformCardList />
-      </InformCardWrapper>
-      <InformHalfCardList>
-        <InformHalfCard
-          iconKind="account"
-          title="취업 길라잡이"
-          subTitle="채용 정보 확인"
-          link="/announcement/recruit/normal"
-        />
-        <InformHalfCard
-          iconKind="language"
-          title="어학 공지사항"
-          subTitle="어학 정보 확인"
-          link="/announcement/language/normal"
-        />
-      </InformHalfCardList>
-      <InformCardWrapper>
-        <InformTitle>비교과</InformTitle>
-        <Carousel />
-      </InformCardWrapper>
+      <SchoolInfo />
+      <ExtraInfo />
+      <Whalbe />
     </Container>
   );
 };
@@ -39,27 +19,7 @@ export default Home;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 85%;
+  width: 90%;
   margin: 0 auto;
-  padding-bottom: 5%;
-`;
-
-const InformCardWrapper = styled.div`
-  overflow: hidden;
-  border-radius: 15px;
-  padding: 5%;
-  background-color: ${THEME.IVORY};
-  margin-top: 5%;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-`;
-
-const InformHalfCardList = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 5%;
-`;
-
-const InformTitle = styled.div`
-  font-size: 20px;
-  font-weight: bold;
+  padding-bottom: 15px;
 `;
